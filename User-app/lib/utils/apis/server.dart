d6560f290d16e153884ef4caa82c2e21d5557323
joeljsv <joelsunny495@gmail.com>
2020-12-13 13:45:48 +0530

    server

--- User-app/lib/utils/apis/server.dart
+++ User-app/lib/utils/apis/server.dart
@@ -1,5 +1,5 @@
 const String CHATBOT_URL="<CHAT BOAT URL>";
 const String MAIN_API_URL="<MEDIC-REST-API-URL>/";
 const String PREDICT_LIFE="<MEDIC-Prediction-API-URL>/";
-const RAZOR_PAY_KEY="rzp_test_ILLB7bOpQyMbqq";
+const RAZOR_PAY_KEY="<RAZOOR_PAY_KEY>";
 const String SERVER_URL = MAIN_API_URL;
